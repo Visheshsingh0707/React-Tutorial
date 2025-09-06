409ff0c4a3cfbdcd793cb9a9b081eb8b112602b7
Vishesh Singh <visheshsingh7700@gmail.com>
2025-09-06 14:20:50 +0530

    adding about section (with switch between light and dark mode)
    Signed-off-by: Vishesh Singh <visheshsingh7700@gmail.com>

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,11 @@ export default function TextForm(props) {
     let newText=text.toLowerCase();
      setText(newText)
     }
+     const handleClearClick=()=>{
+    //  console.log("LowerCase was clicked"+text);
+    let newText='';
+     setText(newText)
+    }
     const handleOnChange=(event)=>{
     //  console.log("on change");
      setText(event.target.value);
@@ -21,10 +26,11 @@ export default function TextForm(props) {
  <div className="container">
      <h1>{props.heading}</h1>
      <div className="mb-3">
-     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+     <textarea className="form-control my-2" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
      {/* it is necessary to write onChange as you are passing text as ur value sou also need to update it using onChange  */}
      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase </button> 
      <button className="btn btn-primary mx-1" onClick={handledownClick}>Convert to LowerCase </button> 
+     <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear All </button> 
      </div> 
 </div>
 <div className="container my-3">
